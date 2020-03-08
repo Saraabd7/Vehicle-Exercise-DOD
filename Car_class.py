49ffdb4b37540b375e5d9723f83a0ffd40dc4c20
Sara Abdrabu <Sabdrabu@tcadmins-air.home>
2020-03-08 19:20:14 +0000

    update

--- Car_class.py
+++ Car_class.py
@@ -1,16 +1,4 @@
-# import my vehicle class
 
-# define car class here and make it inherit from vehicle
-
-
-#Characterists:
-# brand
-# horse_power
-# max_speed
-
-#methods :
-# park
-# honk
 # import my vehicle class
 
 # Define car class here and make it inherit from vehicle
